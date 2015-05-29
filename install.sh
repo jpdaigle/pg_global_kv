a3f0919255b94083fd36c5bdb15977efec726aba
Matthew Kelly <mkelly@tripadvisor.com>
2015-05-29 16:02:03 -0400

    init.d script

--- install.sh
+++ install.sh
@@ -3,7 +3,14 @@
 # Fix working dir
 cd $(dirname $0)
 
+NAME=$(cat settings.gradle | awk '/rootProject.name/ {print $3}' | tr -d "'")
+
 # Gradle install, just builds the dir build/install/pg_global_kv
 ./gradlew install
-rm build/install/pg_global_kv/bin/pg_global_kv.bat
-sudo rsync -a build/install/pg_global_kv /opt/
+rm build/install/${NAME}/bin/${NAME}.bat
+sudo rsync -a build/install/${NAME} /opt/
+
+if [ -d /etc/init.d ]; then
+  rm -f /etc/init.d/${NAME}
+  ln -s /opt/${NAME}/etc/init.d/${NAME} /etc/init.d/${NAME}
+fi
